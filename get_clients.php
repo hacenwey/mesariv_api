69bb0a7e79205d5ba94f49a579dd3e01809f7b97
hacenwey <hacen.wey@awlyg.tech>
2025-08-12 20:41:39 +0000

    remove pass has

--- get_clients.php
+++ get_clients.php
@@ -2,7 +2,7 @@
 require 'config.php';
 
 $user_id = $_GET['user_id'] ?? 0;
-echo password_hash("admin123", PASSWORD_BCRYPT);
+// echo password_hash("admin123", PASSWORD_BCRYPT);
 $stmt = $pdo->prepare("SELECT * FROM clients WHERE user_id = ?");
 $stmt->execute([$user_id]);
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
