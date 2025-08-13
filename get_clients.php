6a3cb8bf136b64aee0862bf01a379bf652d352b3
hacenwey <hacen.wey@awlyg.tech>
2025-08-13 00:07:19 +0000

    get amout

--- get_clients.php
+++ get_clients.php
@@ -3,7 +3,7 @@ require 'config.php';
 
 $user_id = $_GET['user_id'] ?? 0;
 // echo password_hash("admin123", PASSWORD_BCRYPT);
-$stmt = $pdo->prepare("SELECT * FROM clients WHERE user_id = ?");
+$stmt = $pdo->prepare("SELECT c.*, COUNT(t.id) as transactions, SUM(t.amount) as total_amount FROM clients c LEFT JOIN transactions t ON c.id = t.client_id WHERE c.user_id = ? GROUP BY c.id");
 $stmt->execute([$user_id]);
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
