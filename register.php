44034b52d0fc6d482900edb2e8305f4cbfddcb89
hacenwey <hacen.wey@awlyg.tech>
2025-08-15 23:12:44 +0000

    pass word

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ $password = $_GET['password'] ?? '';
 
 if ($phone && $password) {
     $stmt = $pdo->prepare("INSERT INTO users (phone, password) VALUES (?, ?, ?)");
-    $stmt->execute([$phone, password_hash($password, PASSWORD_BCRYPT)]);
+    $stmt->execute([$name,$phone, password_hash($password, PASSWORD_BCRYPT)]);
     echo json_encode(["status" => "success","user" => ["name" => $name,"phone" => $phone, "password" => $password]]);
 } else {
     echo json_encode(["status" => "error", "message" => "Missing phone or password"]);
